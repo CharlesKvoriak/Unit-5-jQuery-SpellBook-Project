c169d2cf19fb5ead6178c7684c5ddc9924666c5c
charliecodespoorly <charliemckennakvoriak@gmail.com>
2019-01-31 16:46:23 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,26 @@
+//Spell #1
+
+
+
+$("#spell1").click(function(){
+    $("#image1").toggle();
+});
+
+
+
+//Spell #2
+
+$("#spell2").click(function(){
+    $("#image2").slideToggle();
+});
+
+
+
+//Spell #3
+
+$("#spell3").click(function(){
+    $("#third").html("<h1>behold my power</h1>");
+});
+
+
+
